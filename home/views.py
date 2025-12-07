023d05ed779216e2e20771d8da3a401775b92423
Manny7536 <shahsubash47@gmail.com>
2025-12-08 01:41:29 +0545

    Initial Django project

---
+++ home/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'index.html')
+
+def results(request):
+    students=[
+        {'name':'Subash Shah', 'age':23, 'ID':'10063','Marks': 86 },
+        {'name':'Prasun Sedai', 'age': 21, 'ID':'10043','Marks': 78 },
+        {'name':'Prasanna Karn', 'age': 19, 'ID':'10098','Marks': 80 },
+        {'name': 'Shashwat Adhakari', 'age': 25, 'ID': '10033','Marks': 91 },
+        {'name': 'Priyanshu Gajurel', 'age': 20, 'ID': '10078','Marks': 75 },
+        {'name': 'Ram ', 'age': 23, 'ID': '10099','Marks': 39 },
+    ]
+    return render(request, 'dt.html', context={'students':students} )

c6aefbe07d6773fb60faec3ec99bf0907ac1bfef
Manny7536 <shahsubash47@gmail.com>
2025-12-08 02:59:34 +0545

    Models/migration

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class StudentsInfo(models.Model):
+    student_id = models.AutoField(primary_key=True)
+    student_name = models.CharField(max_length=100)
+    student_email = models.EmailField(unique=True)
+    student_enrollment = models.BooleanField()
+
